bca1ac28ca09819a6ed02c22da9a3bf10923325d
Andres Vallecilla <andresfelipe083195@hotmail.com>
2025-02-04 16:32:18 -0500

    fix: add mrz data

--- src/core.service.ts
+++ src/core.service.ts
@@ -69,6 +69,7 @@ export class CoreService implements EventHandler, OnModuleInit {
         'credentialIssuanceDate',
         'documentExpirationDate',
         'facePhoto',
+        'mrzData',
       ],
       {
         supportRevocation: true,
